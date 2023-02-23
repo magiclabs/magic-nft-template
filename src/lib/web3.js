2af1956978c95dad66c7c8013e594f1e3db50d35
nickfrosty <nfrostbutter@gmail.com>
2023-02-22 21:55:24 -0500

    refactor: migrated contract address to ENV variable

--- src/lib/web3.js
+++ src/lib/web3.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import { magic } from "./magic";
 import { contractABI } from "./abi";
 
-export const contractAddress = "0x9556470F1dFAc6f72bcE0f46f2487d1D4cb0A30C";
+export const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
 export const web3 = new Web3(magic.rpcProvider);
 
