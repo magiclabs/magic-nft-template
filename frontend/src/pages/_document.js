47610a4ceb79b3bd8d3272fea9ff1a068064affd
James <jamesrp13@gmail.com>
2023-03-01 14:01:28 -0700

    fix: get og image showing (#8)
    * Update _document.js
    
    * Update _document.js
    
    * Update _document.js
    
    * Update og-card image
    
    * Update _document.js

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="twitter:card" content="summary_large_image" key="twcard" />
+        <meta name="twitter:site" content="@magic_labs" key="twhandle" />
+        <meta property="og:type" content="website" key="ogtype" />
         <meta
           property="og:url"
           content={process.env.NEXT_PUBLIC_HOST}
@@ -11,7 +14,7 @@ export default function Document() {
         />
         <meta
           property="og:image"
-          content={`${process.env.NEXT_PUBLIC_HOST}/OG Card - Next.js NFT Template.png`}
+          content={`${process.env.NEXT_PUBLIC_HOST}/og-card.png`}
           key="ogimage"
         />
         <meta
