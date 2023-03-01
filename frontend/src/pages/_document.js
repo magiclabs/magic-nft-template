acb50c670fdf6f08d0d640a22d3853b65fd9cc0f
James Pacheco <jamesrp13@gmail.com>
2023-03-01 11:03:10 -0700

    OG test

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -4,16 +4,31 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
+
+        {/* Open Graph */}
+        {/* <meta property="og:url" content={currentURL} key="ogurl" /> */}
+        <meta
+          property="og:image"
+          content="/OG Card - Next.js NFT Template.png"
+          key="ogimage"
+        />
+        <meta
+          property="og:site_name"
+          content="Magic NFT Demo"
+          key="ogsitename"
+        />
         <meta
           property="og:title"
           content="Next.js NFT Template"
           key="ogtitle"
         />
-        <meta property="og:description" content="" key="ogdesc" />
         <meta
-          property="og:image"
-          content="/OG Card - Next.js NFT Template.png"
-          key="ogimage"
+          property="og:description"
+          content="Clone to get auth, wallet, onramp, minting, viewing, and token gating out of the box."
+          key="ogdesc"
         />
         <title>Next.js NFT Template</title>
       </Head>
